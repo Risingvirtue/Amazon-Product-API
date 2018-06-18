1dca32ad4506b7acbcc293db0037029961edcf80
Your Name <you@example.com>
2018-06-18 15:02:18 -0700

    simple server

--- server.js
+++ server.js
@@ -2,3 +2,20 @@
 
 var express = require('express');
 var app = express();
+
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get("/test", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
+})
